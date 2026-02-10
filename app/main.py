fb6baa55db0c2a519ebdd49e4b38d62cdd6cbe60
Andavarapu Ajit <andavarapu.ajit@gyansys.com>
2026-02-10 11:18:49 +0530

    Added CORS in the main file

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from app.routers import auth, projects, tasks, assignees, task_logs, users, time_entries
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Project Tracking System")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "https://your-frontend-domain.com"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/", tags=["greet"])
 def greet():
